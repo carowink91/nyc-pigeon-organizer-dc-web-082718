4313b8c3642d368e9df07e2bef8beca24e0b140c
angelica winkler <caw400@nyu.edu>
2018-08-11 01:21:21 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,45 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  newHash = {}
+
+  colors = data.fetch(:color)
+  colors.each do |color, pigeons|
+    pigeons.each do |pigeon|
+      newHash[pigeon] = {}
+    end
+  end
+  newHash
+  keys = newHash.keys
+
+  data.each do |attribute, choices|
+    choices.each do |option, names|
+      names.each do |name|
+        i = 0
+        while i < keys.length
+          if name == keys[i]
+            newHash[name][attribute] = []
+            option = option.to_s
+            newHash[name][attribute] << option
+            i += 1
+          else
+            i += 1
+          end
+        end
+      end
+    end
+  end
+  newHash
+
+  colors.each do |color, pigeons|
+    pigeons.each do |pigeon|
+      color = color.to_s
+      if newHash[pigeon][:color].include?(color) == false
+        newHash[pigeon][:color] << color
+      end
+    end
+  end
+
+
+  newHash
 end
